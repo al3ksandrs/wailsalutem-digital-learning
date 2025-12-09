9759ef33d661064f6adf5c581aff2f58109a2e34
groovyraccoon <152175012+groovyraccoon@users.noreply.github.com>
2025-12-09 22:31:36 +0100

    Add components to StudentMainPage as placeholders

--- client/src/pages/StudentMainPage.tsx
+++ client/src/pages/StudentMainPage.tsx
@@ -1,41 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from '../components/Header';
 import ScreenLayout from '../components/ScreenLayout';
 import Notifications from '../components/notifications/NotificationParent';
+import MainInfoPanel from '../components/MainInfoPanel';
+import LogoutButton from '../components/LogoutButton';
+import AvailabilitySlider from '../components/AvailabilitySlider';
+import Modal from '../components/Modal';
 import '../css/student-main.css';
 
 const StudentMainPage: React.FC = () => {
-    return (
-        <ScreenLayout
-            greeting="Goedenavond, Hendrik"
-            rightTitle="Voorgestelde matches"
-            leftContent={
-                <div className="student-page-placeholder">
-                    Hello world :D
-                </div>
-            }
-            rightContent={
-                <div className="student-page-placeholder">
-                    <div>
-                         <Notifications />
-                    </div>
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-                    Hello world :D
-                    Hello world :D
-                    Hello world :D
-                    Hello world :D
-                    Hello world :D
-                    Hello world :D
-                    Hello world :D
-                    <div>
-                        Hello world :D
-                        Hello world :D
-                        Hello world :D
-                        Hello world :D
-                        Hello world :D
-                        Hello world :D
-                        Hello world :D
+    return (
+        <><Header></Header>
+            <ScreenLayout
+                greeting="Goedenavond, Hendrik"
+                rightTitle="Voorgestelde matches"
+                leftContent={
+                    <div className="student-left-panel">
+                        <div className="panel-box">
+                            <MainInfoPanel pending={3} matches={12} connections={27} />
+                        </div>
+                        <LogoutButton onClick={() => setIsModalOpen(true)} />
                     </div>
-                    <div>
+                }
+                rightContent={
+                    <div className="student-page-placeholder">
+                        <Notifications />
+
                         Hello world :D
                         Hello world :D
                         Hello world :D
@@ -43,19 +35,49 @@ const StudentMainPage: React.FC = () => {
                         Hello world :D
                         Hello world :D
                         Hello world :D
+                        <div>
+                            Hello world :D
+                            Hello world :D
+                            Hello world :D
+                            Hello world :D
+                            Hello world :D
+                            Hello world :D
+                            Hello world :D
+                        </div>
+                        <div>
+                            Hello world :D
+                            Hello world :D
+                            Hello world :D
+                            Hello world :D
+                            Hello world :D
+                            Hello world :D
+                            Hello world :D
+                        </div>
+                        <div>
+                            Hello world :D
+                            Hello world :D
+                            Hello world :D
+                            Hello world :D
+                            Hello world :D
+                            Hello world :D
+                            Hello world :D
+                                <AvailabilitySlider 
+                                    day="Maandag"
+                                    value={[9, 17]}
+                                    onChange={(val) => console.log('Maandag availability:', val)}
+                                />
+                        </div>
                     </div>
-                    <div>
-                        Hello world :D
-                        Hello world :D
-                        Hello world :D
-                        Hello world :D
-                        Hello world :D
-                        Hello world :D
-                        Hello world :D
-                    </div>
-                </div>
-            }
-        />
+                }
+            />
+            <Modal
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                title="Weet je zeker dat je wilt uitloggen?"
+            >
+                <p>Als je uitlogt, wordt je sessie beëindigd.</p>
+            </Modal>
+        </>
     );
 };
 
