d16e9d5eb00cbf4896fa8e44a28a9245e245a19c
aleksandrs <soskola@ad.hva.nl>
2025-12-21 14:38:52 +0100

    feat: CORS setup for localhost communication, JWT setup and cookie signing setup

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,9 @@
 import Fastify from 'fastify';
 import fastifyPostgres from '@fastify/postgres';
 import fastifyCookie from '@fastify/cookie';
+import fastifyCors from '@fastify/cors';
+import fastifyJwt from '@fastify/jwt';
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 import { authRoutes } from './routes/auth.ts';
@@ -8,9 +11,31 @@ import { studentRoutes } from './routes/student.js';
 
 dotenv.config();
 
+const PORT = 3000;
+const DB_CHECK_ERROR_CODE = 500;
+
 export const buildServer = () => {
   const fastify = Fastify({
     logger: process.env.NODE_ENV === 'test' ? false : true,
+  });
+
+  // Zod validation
+  fastify.setValidatorCompiler(validatorCompiler);
+  fastify.setSerializerCompiler(serializerCompiler);
+
+  // CORS
+  fastify.register(fastifyCors, {
+    origin: 'http://localhost:5173', // Frontend localhost URL, will need to add frontend VPS origin here later
+    credentials: true,
+  });
+
+  // JWT
+  fastify.register(fastifyJwt, {
+    secret: process.env.JWT_SECRET || 'supersecret', // will probably need to change this in the future to something more unique?
+    cookie: {
+      cookieName: 'token',
+      signed: false, // JWT is already signed
+    },
   });
 
   // Register Postgres plugin
@@ -18,7 +43,10 @@ export const buildServer = () => {
     connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
   });
 
-  fastify.register(fastifyCookie);
+  fastify.register(fastifyCookie, {
+    secret: process.env.COOKIE_SECRET || 'anothersupersecret', // For signing cookies
+    hook: 'onRequest',
+  });
 
   fastify.register(authRoutes, { prefix: '/api/auth' });
 
@@ -37,7 +65,7 @@ export const buildServer = () => {
     } catch (err) {
       (request.log || console).error(err);
       const errorMessage = err instanceof Error ? err.message : 'Unknown error occurred';
-      return reply.code(500).send({ status: 'Database Connection Failed', error: errorMessage });
+      return reply.code(DB_CHECK_ERROR_CODE).send({ status: 'Database Connection Failed', error: errorMessage });
     } finally {
       if (client) client.release();
     }
@@ -53,8 +81,8 @@ export const buildServer = () => {
 const start = async () => {
   const server = buildServer();
   try {
-    await server.listen({ port: 3000, host: '0.0.0.0' });
-    console.log(`Server listening on port 3000`);
+    await server.listen({ port: PORT, host: '0.0.0.0' });
+    console.log(`Server listening on port ${PORT}`);
   } catch (err) {
     console.error(err);
     process.exit(1);
