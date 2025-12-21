ab426c3d54565b311a637318061bb79316b6ee71
RZ2806 <Rida2806@hotmail.com>
2025-12-21 22:28:55 +0100

    test: student requests

---
+++ client/src/tests/pages/StudentRequests.test.tsx
@@ -0,0 +1,67 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { describe, test, expect, vi } from 'vitest';
+import StudentRequests from '../../pages/Student/StudentRequests';
+
+vi.mock('../../components/notifications/NotificationParent', () => ({
+    default: () => <div>NotificationsMock</div>
+}));
+
+describe('StudentRequest', () => {
+    test('renders layout with greeting', () => {
+        render(
+            <MemoryRouter>
+                <StudentRequests />
+            </MemoryRouter>
+        );
+        expect(screen.getByText('Goedenavond, Hendrik')).toBeInTheDocument();
+
+        const titles = screen.getAllByText('Mijn Verzoeken');
+        expect(titles.length).toBeGreaterThan(0);
+        expect(screen.getByRole('heading', { name: 'Mijn Verzoeken' })).toBeInTheDocument();
+    });
+
+    test('renders data', () => {
+
+        render(<StudentRequests />);
+
+        expect(screen.getByText('Scheikude')).toBeInTheDocument();
+        expect(screen.getByText('Wiskunde')).toBeInTheDocument();
+        expect(screen.getByText('Geschiedenis')).toBeInTheDocument();
+
+        expect(screen.getByText('Amsterdam')).toBeInTheDocument();
+        expect(screen.getByText('Alkmaar')).toBeInTheDocument();
+        expect(screen.getByText('Amstelveen')).toBeInTheDocument();
+
+    });
+
+    test('opens logout modal when logout button is clicked', () => {
+        render(
+            <MemoryRouter>
+                <StudentRequests />
+            </MemoryRouter>
+        );
+
+        const logoutBtns = screen.getAllByText('Logout');
+        fireEvent.click(logoutBtns.at(-1)!);
+
+        expect(screen.getByText('Weet je zeker dat je wilt uitloggen?')).toBeInTheDocument();
+    });
+
+    test('closes logout modal', () => {
+        render(
+            <MemoryRouter>
+                <StudentRequests />
+            </MemoryRouter>
+        );
+
+        const logoutBtns = screen.getAllByText('Logout');
+        fireEvent.click(logoutBtns.at(-1)!);
+
+        const closeBtn = document.querySelector('.modal-close');
+        expect(closeBtn).toBeInTheDocument();
+        fireEvent.click(closeBtn!);
+
+        expect(screen.queryByText('Weet je zeker dat je wilt uitloggen?')).not.toBeInTheDocument();
+    });
+});
